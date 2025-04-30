c7964430fae8dd66802ce41120fc44dece47c585
Jose-hashq <Jose.nunes@dawsoncollege.qc.ca>
2025-04-29 20:40:03 -0400

    Mark 1
    Basic code for working the keypad

---
+++ sketch_apr29a/sketch_apr29a.ino
@@ -0,0 +1,32 @@
+#include <Keypad.h>   //The library to use the keypad
+
+const byte ROWS = 4; //The four rows
+const byte COLS = 4; //The four columns
+
+//defining the symbols of the botton keypads
+char hexakeys [ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','C'}
+};
+
+byte rowPins[ROWS] = {9, 8, 7, 6};  //Connecting to row pinouts of keypad
+byte colPins[COLS] = {5, 4, 3, 2};  //connecting the column pinouts of  keypad
+
+//Initialize an instance of class NewKeypad
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  //Get Key value when pressed
+  char customkey = customKeypad.getkey();
+
+  if (customkey) {
+    Serial.println(customkey);
+    Serial.println("Correct, Welcome");
+  }
+}
