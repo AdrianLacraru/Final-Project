36e76d49df5fe2b91728591bf40498d3576d9f4b
Jose <Jose.nunes@dawsoncollege.qc.ca>
2025-05-05 08:58:24 -0400

    Update sketch_apr29a.ino

--- sketch_apr29a/sketch_apr29a.ino
+++ sketch_apr29a/sketch_apr29a.ino
@@ -8,14 +8,14 @@ char hexakeys [ROWS][COLS] = {
   {'1','2','3','A'},
   {'4','5','6','B'},
   {'7','8','9','C'},
-  {'*','0','#','C'}
+  {'*','0','#','D'}
 };
 
 byte rowPins[ROWS] = {9, 8, 7, 6};  //Connecting to row pinouts of keypad
 byte colPins[COLS] = {5, 4, 3, 2};  //connecting the column pinouts of  keypad
 
 //Initialize an instance of class NewKeypad
-Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, [ROWS], [COLS]);
 
 void setup() {
   Serial.begin(9600);
