5b25da5fdfe51a4c172b6f0077933cfab8988d1b
Jose-hashq <Jose.nunes@dawsoncollege.qc.ca>
2025-05-06 19:44:49 -0400

    New Code
    Need work on pinnout and serial print usage

--- sketch_apr29a/sketch_apr29a.ino
+++ sketch_apr29a/sketch_apr29a.ino
@@ -1,58 +1,64 @@
-#include <Keypad.h>   //The library to use the keypad
+#include <Keypad.h>  //The library to use the keypad
 
 //const is a keyword that is specific to a variable and is constant
-const byte ROWS = 4; //The four rows
-const byte COLS = 4; //The four columns
+const byte ROWS = 4;  //The four rows
+const byte COLS = 4;  //The four columns
 
 //defining the symbols of the botton keypads
-char hexakeys [ROWS][COLS] = {
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
+char hexakeys[ROWS][COLS] = {
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
 };
 
-byte rowPins[ROWS] = {A0, A1, A2, A3};  //Connecting to row pinouts of keypad
-byte colPins[COLS] = {5, 4, 3, 2};  //connecting the column pinouts of  keypad
+byte rowPins[ROWS] = { A0, A1, A2, A3 };  //Connecting to row pinouts of keypad
+byte colPins[COLS] = { 5, 4, 3, 2 };      //connecting the column pinouts of  keypad
 
 //Initialize NewKeypad
 Keypad customKeypad = Keypad(makeKeymap(hexakeys), rowPins, colPins, ROWS, COLS);
 
 
-const int Password_Length = 4; //length of our password
-
-String PASSKEY;//holds value
-
-String Master = "1234";//Password
-
+const String correctCode = "1234";  //The 4 digit code that will grant access
+String inputCode = "";
 
 
 void setup() {
-  
-  Serial.begin(9600);
+  Serial.begin(9600);  //Setting up the serial monitor for troubleshooting/debugging
 }
 
 void loop() {
   //Get Key value when pressed
-  char customkey = customKeypad.getKey();
+  char key = customKeypad.getKey();
 
- 
+
+  if (key != NO_KEY) {
+    Serial.print("Key Pressed: "); //Command to the user
+    Serial.println(key);
+  }
+
+  if (key == '#') {  // '#' = submit
+
+    if (inputCode == correctCode) {
+      Serial.println("Access Granted!"); //when code is right
+    } 
+
+    else {
+      Serial.println("Access Denied!"); //When code is wrong
+    }
+    inputCode = "";         // reset input
+   
+  }
+    else if (key == '*') {  // '*' = clear
+      inputCode = "";
+      Serial.println("Input Cleared.");
+    } 
+
+  else if (isDigit(key)) {
+  }  
+
+    if (inputCode.length() < 4) { //if a number between 0-9 are pressed and input is less than 4 charactors it will be outputed
+      inputCode += key;
+    }
   
-
-
-
-
-// 3 - print if it is ok or not
-
- if(Master = "1234"){
-  Serial.println("Correct, you may enter");
- }
- else {
-  Serial.println("Incorrect");
- }
-
-
-  
-
-
 }
